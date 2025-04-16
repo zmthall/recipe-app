ff5d2f6a3542f89b47e8ccb9965771b8a13b39e7
Zack Thallas <zmthall@ZMT-MacBook-Pro.local>
2025-04-15 22:02:59 -0600

    Edited login page background to be rendered with NuxtImg

--- app/components/auth/background.vue
+++ app/components/auth/background.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img src="/images/auth-page.jpg" alt="Food background for login page" class="fixed top-0 left-0 h-screen w-full object-cover">
+    <NuxtImg src="/images/auth-page.jpg" sizes="lg:1600px md:800px xs:600px" fit="cover" quality="60" alt="Food background for login page" format="webp" class="fixed top-0 left-0 h-screen w-full object-cover" />
     <slot />
   </div>
 </template>
